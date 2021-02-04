7156e95c4c9016a5d70233fdd922b9b563087c85
Sergey <timochinsergey@gmail.com>
2021-02-04 22:46:35 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/*alert('Hello');
+
+const result = confirm("Ты тут?");
+
+console.log(result);
+
+const answer = +prompt("Вам есть 18?", "18");
+console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('Как Ваше имя?', '');
+answers[1] = prompt('Как Ваше фамилия?', '');
+answers[2] = prompt('Сколько Вам лет?', '');
+
+document.write(answers);/
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Привет, ${user}`);
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+
+++incr;
+--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === '8');
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);*/   
+
+
